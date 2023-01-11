ea580e85b56eccab2882256fa1b9ba022d0cc789
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-01-11 21:30:43 +0600

    Homework - 1

---
+++ homework_1/Geek/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Geek project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Geek.settings')
+
+application = get_asgi_application()
