19b92ca3ee85753ba6498e5465a0dd30c223f902
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-01-11 21:42:17 +0600

    Исправление ошибок

--- homework_1/Geek/settings.py
+++ homework_1/Geek/settings.py
@@ -18,8 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 from dotenv import load_dotenv
 
-load_dotenv(os.path.join(BASE_DIR, ".env")
-)
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
